94f1b15807d88c7ce98a0b7c683e48f65c368c28
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-10-01 13:36:54 +0530

    synchronous programming

---
+++ Asynchronous Programming Basics/1_synchronous_programming.js
@@ -0,0 +1,17 @@
+// Syncronous programming 
+// Not efficient 
+console.log('Start');
+function1();
+function2();
+console.log('End');
+
+//Time taking function 
+function function1(){
+    // Loading data from an API
+    // Uploading files
+    // Animations 
+}
+
+function function2(){
+    console.log(100 + 50);
+}

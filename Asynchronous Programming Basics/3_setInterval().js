22b6fa86494386e941ba8ea86f413a4402ad6573
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-10-01 13:44:48 +0530

    setInterval() Method

---
+++ Asynchronous Programming Basics/3_setInterval().js
@@ -0,0 +1,9 @@
+
+console.log('start');
+
+// setInterval() Method : Repeat task after every 3 second (time delay)
+setInterval(function(){
+    console.log('Executing task\n');
+}, 1000);
+
+console.log('end');

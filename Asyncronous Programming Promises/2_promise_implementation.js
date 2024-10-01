98af1e55e997d819a65ccefe1bf28d702cefc221
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-10-01 17:01:38 +0530

    implementation of promises

---
+++ Asyncronous Programming Promises/2_promise_implementation.js
@@ -0,0 +1,28 @@
+// Create a promise using promise constructor 
+const myPromise = new Promise((resolve, reject) => {
+
+    // Set a timeOut() of 1 second
+    setTimeout(() => {
+        // Generate a random number between 0-9
+        const randomNum = Math.floor(Math.random() * 10);
+
+        // resolve the promise 
+        if(randomNum < 5){
+            resolve(`Success! Random Number: ${randomNum}`);
+        }
+        // reject the promise 
+        else{
+            reject(`Error! Random Number: ${randomNum}`);
+        }
+    }, 1000); 
+});
+
+
+myPromise
+    .then((result)=> {
+        console.log(result);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+

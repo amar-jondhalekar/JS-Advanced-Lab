552cbac8f068cd120403c5d8b058857222b72ee2
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-10-01 17:01:06 +0530

    promises in javascript

---
+++ Asyncronous Programming Promises/1_promise.js
@@ -0,0 +1,6 @@
+
+const promise = new Promise((resolve, reject) => {
+    // Perform asynchronous operation for eg: setTimeout()
+    // Call 'resolve' function when the operation succeeds 
+    // Call 'reject' function when the operation encounters and error 
+});
